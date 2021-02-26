b1a3cf3412e02d6d4487118e66007d647c7351bf
Noor Mahini <nmahini@berkeley.edu>
2021-02-26 03:35:34 -0800

    first commit with code

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float scrollSpeed;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 camPos = this.transform.position;
+        this.transform.position = new Vector3(camPos.x + scrollSpeed, camPos.y, camPos.z);
+    }
+}
---
+++ Assets/Scripts/Phrog.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Phrog : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float rotateSpeed;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(0.0f, 0.0f, -Input.GetAxis("Horizontal") * rotateSpeed);
+    }
+}
